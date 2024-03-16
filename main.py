683b79fc30b6fe2557134b8c44917b81f0a1e241
PAPAISME <31824176+PAPAISME@users.noreply.github.com>
2024-03-16 23:11:39 +0800

    Update main.py
    Add get_dynamic_fork_parameter function

--- main.py
+++ main.py
@@ -6,9 +6,42 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def hello_world():
     return {"Hello": "World"}
 
+
+@app.get("/api/")
+def get_dynamic_fork_parameter():
+    return {
+        "dynamictasks": [
+            {
+                "subWorkflowParam": {
+                    "name": "MaSubWorkflow_Test",
+                    "version": "1",
+                },
+                "type": "SUB_WORKFLOW",
+                "taskReferenceName": "MaSubWorkflow_Test_F12KK8",
+            },
+            {
+                "subWorkflowParam": {
+                    "name": "MaSubWorkflow_Test",
+                    "version": "1",
+                },
+                "type": "SUB_WORKFLOW",
+                "taskReferenceName": "MaSubWorkflow_Test_F12KK9",
+            },
+        ],
+        "dynamicTasksInput": {
+            "MaSubWorkflow_Test_F12KK8": {
+                "dbname": "F12KK8",
+            },
+            "MaSubWorkflow_Test_F12KK9": {
+                "dbname": "F12KK9",
+            },
+        },
+    }
+
+
 @app.get("/api/{dbname}")
-def read_root(dbname: str):
+def get_dbname(dbname: str):
     return {"dbname": dbname}
